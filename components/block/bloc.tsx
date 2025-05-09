ba68fe304f55808a7d424ec34b7b7c58f5b7d485
yassine bziouech <yassinebziouech22@gmail.com>
2025-05-09 18:57:41 +0100

    update

--- components/block/bloc.tsx
+++ components/block/bloc.tsx
@@ -8,10 +8,10 @@ const Bloc: React.FC = () => {
       <div className="content">
         <div className="textBlock">
         <h2 className="title">
-          Découvrez l’interface intuitive et sécurisée de My-Drive
+          Découvrez l’interface intuitive <br />et sécurisée de My-Drive
         </h2>
         <h3 className="subtitle">
-          Une expérience fluide pour gérer, organiser et protéger vos fichiers
+          Une expérience fluide pour gérer, organiser <br />et protéger vos fichiers
         </h3>
         </div>
         <div className="imageBlock">
